43237c5388e1a9894bb658a3c2d0742592f23f7e
henrique vaz <henriquevaz7@hotmail.com>
2025-04-21 11:53:56 +0100

    fix

--- miradourum/src/main/java/pt/uminho/di/aa/miradourum/repositories/UserRepository.java
+++ miradourum/src/main/java/pt/uminho/di/aa/miradourum/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-package pt.uminho.di.aa.miradourum.repository;
+package pt.uminho.di.aa.miradourum.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pt.uminho.di.aa.miradourum.models.User;
