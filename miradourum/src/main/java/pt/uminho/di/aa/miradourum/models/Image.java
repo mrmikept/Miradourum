050296e01b9d0e06215adaf39b49f88dd322df3d
henrique vaz <henriquevaz7@hotmail.com>
2025-04-21 11:25:55 +0100

    image model

--- miradourum/src/main/java/pt/uminho/di/aa/miradourum/models/Image.java
+++ miradourum/src/main/java/pt/uminho/di/aa/miradourum/models/Image.java
@@ -1,4 +1,47 @@
 package pt.uminho.di.aa.miradourum.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private Long id;
+
+    @Column(name="Url")
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "ReviewID",referencedColumnName = "ID")
+    private Review review;
+
+
+    public Image(Review review) {
+        this.review = review;
+    }
+
+    public Image() {}
+
+
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
