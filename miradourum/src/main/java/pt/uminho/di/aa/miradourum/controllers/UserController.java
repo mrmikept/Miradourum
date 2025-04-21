804620bc31194ce015abc98aff7e11a241d3bd87
mayagomes039 <a100822@alunos.uminho.pt>
2025-04-21 22:36:52 +0100

    user controller

--- miradourum/src/main/java/pt/uminho/di/aa/miradourum/controllers/UserController.java
+++ miradourum/src/main/java/pt/uminho/di/aa/miradourum/controllers/UserController.java
@@ -27,4 +27,45 @@ public class UserController {
         return ResponseEntity.ok(pontoInteresseService.getNewest("merda",123,12.3, LocalDateTime.now(),12,false,1));
     }
 
+    // Get user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        User user = userService.getUserById(id);
+        if (user == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user);
+    }
+
+    // Create new user Post("/register")
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        if (userService.emailExists(user.getEmail())) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+        userService.saveUser(user);
+        return ResponseEntity.ok("User created successfully");
+    }
+
+    // Update user
+    @PutMapping("/{id}/update")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        User existingUser = userService.getUserById(id.intValue());
+        if (existingUser == null) return ResponseEntity.notFound().build();
+
+        updatedUser.setId(id);
+        userService.saveAndUpdateUser(updatedUser);
+        return ResponseEntity.ok("User updated successfully");
+    }
+
+    // Check if user is premium
+    @GetMapping("/{id}/premium")
+    public ResponseEntity<Boolean> isPremium(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.checkPremium(id));
+    }
+
+    // Get image URLs dos pontos visitados
+    @GetMapping("/{id}/images")
+    public ResponseEntity<List<String>> getImageUrls(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getImagesURL(id));
+    }
+
 }
