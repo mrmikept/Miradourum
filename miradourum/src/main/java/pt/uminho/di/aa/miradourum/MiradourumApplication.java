68e4d5560b5b5a54a56133bef422f78500c3ba00
Gonçalo Marinho <a90969@alunos.uminho.pt>
2025-04-21 10:10:06 +0100

    Initial Repo

---
+++ miradourum/src/main/java/pt/uminho/di/aa/miradourum/MiradourumApplication.java
@@ -0,0 +1,13 @@
+package pt.uminho.di.aa.miradourum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiradourumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiradourumApplication.class, args);
+    }
+
+}
---
+++ miradourum/src/test/java/pt/uminho/di/aa/miradourum/MiradourumApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.uminho.di.aa.miradourum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiradourumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
