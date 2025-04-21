38e8e3142f19cfaa3dd4e36ca8aacdc133ac90a8
Mike Pinto <mike.pinto1507@gmail.com>
2025-04-21 11:48:06 +0100

    Added PontoInteresse Repository

---
+++ miradourum/src/main/java/pt/uminho/di/aa/miradourum/repository/PontoInteresseRepository.java
@@ -0,0 +1,7 @@
+package pt.uminho.di.aa.miradourum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pt.uminho.di.aa.miradourum.models.PontoInteresse;
+
+public interface PontoInteresseRepository extends JpaRepository<PontoInteresse, Long> {
+}
